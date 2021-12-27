7e3d306d3ee42a222d9ba3ab05b40387a4955a13
oshi-yuki <92504834+oshi-yuki@users.noreply.github.com>
2021-12-27 15:37:24 +0900

    Update wsgi.py

--- ltS/wsgi.py
+++ ltS/wsgi.py
@@ -8,11 +8,9 @@ https://docs.djangoproject.com/en/3.2/howto/deployment/wsgi/
 """
 
 import os
-from Whitenoise.django import DjangoWhiteNoise
 
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ltS.settings')
 
 application = get_wsgi_application()
-application = DjangoWhiteNoise(application)
